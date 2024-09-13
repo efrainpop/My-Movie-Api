44344fe5f3b802ade53d12d6595dfbc2d46445ef
Efrain Chaguendo <efra.acm@gmail.com>
2024-09-12 20:24:12 -0500

    abordamos la seccion 1 de fast api

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI #importa la clase fastapi de la libreria fastapi
-
+from fastapi.responses import HTMLResponse #importa la clase HTMLResponse de la libreria fastapi
+from movies_list import movies_list
 app = FastAPI() #crea una instancia de la clase fastapi
 app.title = "Mi primera aplicacion de peliculas y Analisis de Datos"
 app.version = "0.0.1"
 @app.get('/', tags=['Home']) #definiendo una ruta
 def message():# definimos una funcion de la ruta
-    return  'Hello World' #devolvemos un diccionario
+    return  HTMLResponse('<h1>Hola mundo</h1>') #retorna un objeto de la clase HTMLResponse
+
+@app.get('/movies', tags=['Movies']) #definiendo una ruta
+def movies():# definimos una funcion de la ruta
+    return  movies_list
