fa7bf57d94e281d8b731386f760681e089582df8
Efrain Chaguendo <efra.acm@gmail.com>
2024-09-15 21:46:23 -0500

    abordamos la seccion 3 de fast api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI #importa la clase fastapi de la libreria fastapi
+from fastapi import FastAPI, Body #importa la clase fastapi de la libreria fastapi
 from fastapi.responses import HTMLResponse #importa la clase HTMLResponse de la libreria fastapi
 from movies_list import movies_list
 app = FastAPI() #crea una instancia de la clase fastapi
@@ -13,8 +13,24 @@ def movies():# definimos una funcion de la ruta
     return  movies_list
 
 @app.get('/movies/{id}', tags=['Movies']) #app get consultar por id
-def get_movie(id:int):# definimos una funcion de la ruta
+def get_movies(id: int):# definimos una funcion de la ruta
     for item in movies_list:
-        if item['id'] == id:
+        if item["id"] == id:
             return item
     return[]
+
+@app.get('/movies/', tags=['Movies']) #definiendo una ruta
+def get_movies_by_category(category: str = "All", year: int = 0):# definimos una funcion de la ruta #app get consultar por categoria y anio():# definimos una funcion de la ruta
+    return [ item for item in movies_list if item["category"] == category and item["year"] == year]
+
+@app.post('/movies/', tags=['Movies']) #definiendo una ruta
+def create_movie(movie: dict = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):# definimos una funcion de la ruta
+    movies_list.append({
+        "id": len(movies_list) + 1,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies_list
